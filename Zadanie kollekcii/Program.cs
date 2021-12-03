e0f22e79d54cdbf1ce9cf6f9a6381902318f67c8
NikolaiKruchkov <47074017+NikolaiKruchkov@users.noreply.github.com>
2021-12-03 15:51:28 +0300

    Add files via upload

---
+++ Zadanie kollekcii/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Zadanie_kollekcii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку, затем нажмите Enter");
+            string str = Console.ReadLine();
+            str.ToCharArray();
+            Stack<char> stack = new Stack<char>();
+            foreach (char i in str)
+            {
+                if (i == '(')
+                {
+                    stack.Push(')');
+                }
+                if (i == '{')
+                {
+                    stack.Push('}');
+                }
+                if (i == '[')
+                {
+                    stack.Push(']');
+                }
+                if (i == ')'|| i == '}' || i == ']')
+                {
+                    if (stack.Count != 0)
+                    {
+                        if (stack.Peek() == i)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                    else
+                    {
+                        stack.Push(i);
+                    }
+                }
+            }
+            if (stack.Count == 0)
+            {
+                Console.WriteLine("В строке скобки расставлены корректно");
+            }
+            else
+            {
+                Console.WriteLine("В строке скобки расставлены некорректно");
+            }
+
+        }
+    }
+}
